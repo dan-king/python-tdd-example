92a521683fb248aabe063c8b9609614d34067bcc
Dan King <dan.king@vptech.io>
2021-06-21 16:55:57 -0700

    Test HTML via comparison to template

--- lists/tests.py
+++ lists/tests.py
@@ -1,6 +1,6 @@
 from django.urls import resolve
 from django.test import TestCase
-from django.http import HttpRequest
+from django.template.loader import render_to_string
 
 from lists.views import home_page
 
@@ -11,12 +11,8 @@ class HomePageTest(TestCase):
         self.assertEqual(found.func, home_page)
 
     def test_home_page_returns_correct_html(self):
-        request = HttpRequest()
-        response = home_page(request)
-        html = response.content.decode('utf8')
-        self.assertTrue(html.startswith('<html>'))
-        self.assertIn('<title>To-Do lists</title>',  html)
-        self.assertTrue(html.strip().endswith('</html>'))
+        response = self.client.get('/')
+        self.assertTemplateUsed(response, 'home.html')
 class SmokeTest(TestCase):
     def test_good_maths(self):
         self.assertEqual(1 + 1, 2)
