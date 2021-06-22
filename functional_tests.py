904fec7ef43a1afafadf9bf3105fc102c504020d
Dan King <dan.king@vptech.io>
2021-06-21 17:23:45 -0700

    Test for HTML header, input and table elements; add those objects to the static HTML file

--- functional_tests.py
+++ functional_tests.py
@@ -1,5 +1,6 @@
 from selenium import webdriver
-
+from selenium.webdriver.common.keys import Keys
+import time
 import unittest
 
 class NewVisitorTest(unittest.TestCase):
@@ -9,7 +10,8 @@ class NewVisitorTest(unittest.TestCase):
         self.browser = webdriver.Firefox()
 
     def tearDown(self):
-        self.browser.quit()
+        if False:
+            self.browser.quit()
 
     # Custom method to ensure browser opened to expected page
     def test_can_start_a_list_and_retrieve_it_later(self):
@@ -23,6 +25,29 @@ class NewVisitorTest(unittest.TestCase):
         # Conform that the page has main heading matching text as coded
         header_text = self.browser.find_element_by_tag_name('h1').text
         self.assertIn('To-Do', header_text)
+
+        # Confirm there is an input field with expected placeholder value
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertEqual(
+            inputbox.get_attribute('placeholder'), 'Enter a to-do item'
+        )
+
+        # Enter a value into the input field
+        inputbox.send_keys('Find a feather')
+
+        # Hit return on the input field
+        inputbox.send_keys(Keys.ENTER)
+
+        # Wait a moment for the page to update
+        time.sleep(1)
+
+        # After hitting enter the page loads showing the new value that was just entered.
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertTrue(
+            any(row.text == 'Find a feather' for row in rows),
+            "The new to-do item was not found in the output list."
+        )
 
         # Fail on purpose as reminder to finish writing tests
         self.fail('TODO: Finish writing tests.')
