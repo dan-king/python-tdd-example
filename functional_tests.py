8ec260a1d626c300027e2ce986565728a177f54d
Dan King <dan.king@vptech.io>
2021-06-22 18:12:07 -0700

    Add test for second entry

--- functional_tests.py
+++ functional_tests.py
@@ -13,6 +13,11 @@ class NewVisitorTest(unittest.TestCase):
         if False:
             self.browser.quit()
 
+    def check_for_row_text_in_table(self, row_text):
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn(row_text, [row.text for row in rows])
+
     # Custom method to ensure browser opened to expected page
     def test_can_start_a_list_and_retrieve_it_later(self):
 
@@ -32,6 +37,9 @@ class NewVisitorTest(unittest.TestCase):
             inputbox.get_attribute('placeholder'), 'Enter a to-do item'
         )
 
+        # ########################################
+        # Add first entry
+        # ########################################
         # Enter a value into the input field
         inputbox.send_keys('Find a feather')
 
@@ -50,6 +58,18 @@ class NewVisitorTest(unittest.TestCase):
         #     f"\nContents were\n {table.text}"
         # )
         self.assertIn('1. Find a feather', [row.text for row in rows])
+
+        # ########################################
+        # Add second entry
+        # ########################################
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Find a cap')
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.check_for_row_text_in_table('1. Find a feather')
+        self.check_for_row_text_in_table('2. Find a cap')
 
         # Fail on purpose as reminder to finish writing tests
         self.fail('TODO: Finish writing tests.')
