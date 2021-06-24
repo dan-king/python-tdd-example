d9e54e1c5812cde9d013ab604ff99896ef8a1e0b
Dan King <dan.king@vptech.io>
2021-06-23 19:33:04 -0700

    Implement a list model to be the parent object of each item

--- lists/tests.py
+++ lists/tests.py
@@ -1,7 +1,7 @@
 from django.urls import resolve
 from django.test import TestCase
 from django.template.loader import render_to_string
-from lists.models import Item
+from lists.models import Item, List
 from lists.views import home_page
 
 class HomePageTest(TestCase):
@@ -24,8 +24,9 @@ class ListViewTest(TestCase):
         self.assertTemplateUsed(response, 'list.html')
 
     def test_displays_all_list_items(self):
-        Item.objects.create(text='Some item 1')
-        Item.objects.create(text='Some item 2')
+        list_ = List.objects.create()
+        Item.objects.create(text='Some item 1', list=list_)
+        Item.objects.create(text='Some item 2', list=list_)
 
         response = self.client.get('/lists/the-only-list-in-the-world/')
 
@@ -42,27 +43,38 @@ class NewListTest(TestCase):
     def test_redirects_after_POST(self):
         response = self.client.post('/lists/new', data={'item_text': 'A new list item'})
         self.assertEqual(response.status_code, 302)
-        self.assertEqual(response['location'], '/lists/the-only-list-in-the-world')
+        self.assertEqual(response['location'], '/lists/the-only-list-in-the-world/')
         #self.assertRedirects(response, '/lists/the-only-list-in-the-world')
 
 
-class ItemModelTest(TestCase):
+class ListAndItemModelsTest(TestCase):
 
     def test_saving_and_retrieving_items(self):
+        list_ = List()
+        list_.save()
+
         first_item = Item()
         first_item.text = 'The first (ever) list item'
+        first_item.list = list_
         first_item.save()
 
         second_item = Item()
         second_item.text = 'Dis da second'
+        second_item.list = list_
         second_item.save()
         
+        saved_list = List.objects.first()
+        self.assertEqual(saved_list, list_)
+
         saved_items = Item.objects.all()
         self.assertEqual(saved_items.count(), 2)
+
         first_saved_item = saved_items[0]
         second_saved_item = saved_items[1]
         self.assertEqual(first_saved_item.text, first_item.text)
+        self.assertEqual(first_saved_item.list, list_)
         self.assertEqual(second_saved_item.text, second_item.text)
+        self.assertEqual(second_saved_item.list, list_)
 
 class SmokeTest(TestCase):
     def test_good_maths(self):
--- lists/views.py
+++ lists/views.py
@@ -1,6 +1,6 @@
 from django.http import HttpResponse
 from django.shortcuts import redirect, render
-from lists.models import Item
+from lists.models import Item, List
 
 # Create your views here.
 def home_page(request):
@@ -36,8 +36,9 @@ def home_page(request):
     # })
 
 def new_list(request):
-    Item.objects.create(text=request.POST['item_text'])
-    return redirect('/lists/the-only-list-in-the-world')
+    list_ = List.objects.create()
+    Item.objects.create(text=request.POST['item_text'], list=list_)
+    return redirect('/lists/the-only-list-in-the-world/')
 
 def view_list(request):
     items = Item.objects.all()        
