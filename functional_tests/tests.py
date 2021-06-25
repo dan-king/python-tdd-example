b2fb8fc8d5240056cf74607dc7149d8c4f19e8a1
Dan King <dan.king@vptech.io>
2021-06-25 13:36:57 -0700

    Test for text box center alignment

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -131,6 +131,29 @@ class NewVisitorTest(LiveServerTestCase):
         # Fail on purpose as reminder to finish writing tests
         #self.fail('TODO: Finish writing tests.')
 
+    def test_layout_and_styling(self):
+        # Open site at specific size
+        self.browser.get(self.live_server_url)
+        self.browser.set_window_size(1024, 768)
+
+        # Ensure the input box is centered
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertAlmostEqual(
+            inputbox.location['x'] + inputbox.size['width'] / 2,
+            512,
+            delta=10
+        )
+
+        # Ensure the input box is centered on the 'new list' page
+        inputbox.send_keys('testing')
+        inputbox.send_keys(Keys.ENTER)
+        self.wait_for_row_text_in_table('1. testing')
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertAlmostEqual(
+            inputbox.location['x'] + inputbox.size['width'] / 2,
+            512,
+            delta=10
+        )
 
 
 # #################################
